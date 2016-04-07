ac3a17bc7367c2dbc57f23031007a4b4339dd013
McDonnell-Lab <mark.mcdonnell@unisa.edu.au>
2016-04-07 16:29:53 +0930

    Changed some file names
    Changed names to reflect they are general optimisation routines.

--- MNIST_Driver.m
+++ MNIST_Driver.m
@@ -2,19 +2,27 @@ clear all
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%this is an example script that implements an ELM network to classify images
-%The main computation required is the calculation of output weights.
+%This is an example script that implements computation of least squares optimal computation of weights matrices, 
+%such as for application in single-hidden layer neural networks with a linear output layer, applied to
+%classifying images, as in refs [1] and [2] below.
+%
+%The main computation required is the optimisation of output weights to minimise mean square error between training targets and data.
+%
 %Several methods for doing this are provided.
-% 1. The standard and generally fastest method is to solve linear equations using a Cholesky solver
-% 2. The main deficiency in the standard method is its high memory burden
-%    for large hidden layers. Three methods here can circumvent this: the
-%    conjugate gradient method, the modular method, and the epsilon-NLMS method
-% 3. The standard method also is not good for non-stationary data. The RLS
-%    and epsilon-NLMS methods are better for non-stationary data.
+% 1. The generally simplest method is to solve for all training data in a
+% single batch using linear equations using a Cholesky solver.
+% 2. The main deficiency in the single-batch Cholesky method is its high memory burden
+%    for large hidden layers. Four methods here can circumvent this: the
+%    conjugate gradient method, the modular method,  the epsilon-NLMS
+%    method and the `Big Data' method.
+% 3. The standard Cholesky method also is not good for non-stationary data. The RLS
+%    and epsilon-NLMS methods are better for non-stationary data, as in the OPIUM and OPIUM-light methods (see comments in E_NLMS.m).
 % 4. The modular method offers several advantages: less memory intensive,
 %    and also faster than the standard method for the same total number of
 %    hidden units. However it loses some accuracy and generally a higher number of hidden units is
 %    necessary to get close to the same performance.
+%5.  The 'Big Data' method simply iteratively computes the Gram matrix for
+%    use in the standard Cholesky solver approach.
 
 %Author: A/Prof Mark D. McDonnell, University of South Australia
 %Email: mark.mcdonnell@unisa.edu.au
@@ -32,6 +40,13 @@ clear all
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%This example script loads the MNIST training and test images, trains the output layer of a
+%single hidden layer neural network with a linear output layer, with the
+%assumption of random or shaped (see ref [1]) untrained input layer weights.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Step 1: load and prepare MNIST data
@@ -51,11 +66,11 @@ RF_Border = 3; %used only if InputWeightFlags(1) == 1
 Scaling = 2;
 
 %parameters for hidden units
-M=3200;  %number of hidden units
+M=800;  %number of hidden units
 HiddenUnitType = 'Relu'; %options: 'Relu', 'Sigmoid', 'Quadratic', 'Tanh', 'Relu','Cubic','SignedQuadratic'
 
 %parameters for output weights: there are different choices of optimisation method
-LearningMethod = 'SingleBatchRidgeRegression' %options: 'SingleBatchRidgeRegression', 'ConjGrad', 'Modular', 'RLS', 'e-NLMS','BigData'
+LearningMethod = 'BigData' %options: 'SingleBatchRidgeRegression', 'ConjGrad', 'Modular', 'RLS', 'e-NLMS','BigData'
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Step 2: get input layer weights
@@ -87,32 +102,32 @@ switch LearningMethod
     case 'BigData'
         Lambda = 1e-2;
         BatchSize = 10000;
-        [W_outputs] = BigData_ELM(A,Y,M,Lambda, BatchSize,NumClasses,k_train);
+        [W_outputs] = BigData_BatchRegression(A,Y,M,Lambda, BatchSize,NumClasses,k_train);
     case 'ConjGrad'
-        %The conjugate gradient method.
+        %The conjugate gradient method applied to the normal equations
         MaxIterations = 100;
         ProgressFlag = 0;
         Lambda = 1e-8; %ridge regression/weight decay regularisation parameter
-        W_outputs = ConjGrad_ELM(A,A_test,NumClasses,k_train,k_test,Y,labels,labels_test,Lambda,M,MaxIterations,ProgressFlag);
+        W_outputs = ConjGrad_NormalEquations(A,A_test,NumClasses,k_train,k_test,Y,labels,labels_test,Lambda,M,MaxIterations,ProgressFlag);
     case 'RLS'
         %Iteratively solve for the output weights using multiple batches of training samples and the Recursive-Least-Squares equations   
         BatchSize = 100; %seems to be fastest batchsize
         Lambda = 1e-2; % a larger Lambda seems necessary for larger batch sizes  
-        [W_outputs,P] = RLS_ELM(A,Y,M,Lambda,BatchSize,NumClasses,k_train);  
+        [W_outputs,P] = RLS(A,Y,M,Lambda,BatchSize,NumClasses,k_train);  
     case 'Modular'
         %Incrementally solve for the output weights using multiple batches of hidden units, i.e. modules.
         ModuleSize = 800;
         ProgressFlag =1;
         Lambda = 1e-2;
         StoppingValue = 1e-6;
-        [W_outputs,Y_predicted_train,Y_predicted_test] = Modular_ELM(X,X_test,NumClasses,k_train,k_test,HiddenUnitType,W_input,Y,labels,labels_test,Lambda,M,ModuleSize,ProgressFlag,StoppingValue);
+        [W_outputs,Y_predicted_train,Y_predicted_test] = Modular_Regression(X,X_test,NumClasses,k_train,k_test,HiddenUnitType,W_input,Y,labels,labels_test,Lambda,M,ModuleSize,ProgressFlag,StoppingValue);
     case 'e-NLMS'
         %solve approximately using regularised normalised LMS algorithm, without requiring an M x M matrix
         BatchSize = 100; 
         Lambda = 1; 
         Delta = 0.1;
         Runs = 4;
-        W_outputs = E_NLMS_ELM(A,Y,M,Lambda,BatchSize,NumClasses,k_train,Delta,Runs);
+        W_outputs = E_NLMS(A,Y,M,Lambda,BatchSize,NumClasses,k_train,Delta,Runs);
     otherwise
         disp('No method selected')
         return
@@ -136,4 +151,5 @@ if ~strcmp(LearningMethod,'Modular')
 end
 [~,ClassificationID_test] = max(Y_predicted_test); %get output layer response and then classify it
 PercentCorrect_test = 100*(1-length(find(ClassificationID_test-1-labels_test'~=0))/k_test)
+
 
